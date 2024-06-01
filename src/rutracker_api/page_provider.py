cd1bcac2a3c45187bf2e28b6e4018b8b0be1cbfc
aliceglass <abayhanov1998@gmail.com>
2024-06-01 12:23:07 +0300

    Вынес заголовки в инициализатор класса, добавил методы для поиска капчи и получения статуса пользователя

--- src/rutracker_api/page_provider.py
+++ src/rutracker_api/page_provider.py
@@ -1,4 +1,4 @@
-from .utils import get_captcha, is_autorized, is_captcha, save_captcha
+from .utils import extract_user_info, get_captcha, get_profile_url, is_autorized, search_captcha
 from .enums import Url
 from .exceptions import (
     AuthorizationException,
@@ -15,34 +15,34 @@ class PageProvider:
         self.session = session
         self.authorized = False
         self.base_url = 'https://rutracker.org/forum/'
-
-    def login(self, username, password, proxy: str | None = None):
-        login_url = f"{self.base_url}login.php"
-
-        # Заголовки, чтобы имитировать браузер
-        headers = {
+        self.headers = {
             'User-Agent': Url.USER_AGENT.value
         }
+        self.profile_url = None
+
+    def search_captcha(self):
+        captcha_img_url = search_captcha(self.session.get(Url.LOGIN_URL.value, headers=self.headers))
+        if captcha_img_url:
+            return get_captcha(self.session, captcha_img_url)
+
+    def login(self, username, password, captcha=None):
+        login_url = Url.LOGIN_URL.value
 
         # Параметры логина
         payload = {
             'login_username': username,
             'login_password': password,
+            'login_captcha': captcha,
             'login': 'Вход'
         }
 
         # Отправка POST-запроса для логина
-        response = self.session.post(login_url, data=payload, headers=headers)
+        response = self.session.post(login_url, data=payload, headers=self.headers)
 
-        # Проверка на наличие капчи
-        captcha_tag = is_captcha(response)
-        if captcha_tag:
-            save_captcha(self.session, captcha_tag)
-            captcha_img = get_captcha()
-            return captcha_img
         # Проверка успешной авторизации
         if is_autorized(response, username):
             self.authorized = True
+            self.profile_url = get_profile_url(response)
             return 'success'
 
     def search(self, query, sort, order, page):
@@ -51,11 +51,6 @@ class PageProvider:
 
         search_url = f"{self.base_url}tracker.php"
 
-        # Заголовки, чтобы имитировать браузер
-        headers = {
-            'User-Agent': Url.USER_AGENT.value
-        }
-
         params = {
             'nm': query,
             's': sort,
@@ -63,7 +58,8 @@ class PageProvider:
             'start': (page - 1) * 50
         }
 
-        response = self.session.get(search_url, params=params, headers=headers)
+        response = self.session.get(search_url, params=params,
+                                    headers=self.headers)
         if response.status_code != 200:
             raise ServerException(f"Ошибка выполнения поиска: {response.status_code}")
 
@@ -75,13 +71,13 @@ class PageProvider:
 
         download_url = f"{Url.DOWNLOAD_URL.value}?t={topic_id}"
 
-        # Заголовки, чтобы имитировать браузер
-        headers = {
-            'User-Agent': Url.USER_AGENT.value
-        }
-
-        response = self.session.get(download_url, headers=headers)
+        response = self.session.get(download_url, headers=self.headers)
         if response.status_code != 200:
             raise ServerException(f"Ошибка загрузки торрент-файла: {response.status_code}")
 
         return response.content
+
+    def status(self):
+        response = self.session.get(self.profile_url, headers=self.headers)
+        user_info = extract_user_info(response)
+        return user_info
