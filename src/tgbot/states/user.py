b4772f590afdd96d570bd120f0ac59f41dc1fee0
aliceglass <abayhanov1998@gmail.com>
2024-06-01 13:07:43 +0300

    Добавил состояние send_captcha в группу состояний LoginFSM

--- src/tgbot/states/user.py
+++ src/tgbot/states/user.py
@@ -4,6 +4,7 @@ from aiogram.fsm.state import State, StatesGroup
 class LoginFSM(StatesGroup):
     send_username = State()
     send_password = State()
+    send_captcha = State()
     submit = State()
 
 
