f9b40380696c9cb3a6d51fe530ebed6c949daad5
aliceglass <abayhanov1998@gmail.com>
2024-06-01 13:03:40 +0300

    Добавил генератор клавиатуры с пагинацией и кнопками скачать и отмена, добавил клавиатуру главного меню бота

--- src/tgbot/keyboards/keyboards.py
+++ src/tgbot/keyboards/keyboards.py
@@ -1,5 +1,9 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram import Bot
+from aiogram.types import (InlineKeyboardMarkup,
+                           InlineKeyboardButton, BotCommand)
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from tgbot.lexicon.lexicon import LEXICON, LEXICON_COMMANDS
 
 
 def create_submit_kb() -> InlineKeyboardMarkup:
@@ -13,27 +17,33 @@ def create_submit_kb() -> InlineKeyboardMarkup:
     return kb_builder.as_markup()
 
 
-def create_torrents_kb(results: dict) -> InlineKeyboardMarkup:
+# Функция, генерирующая клавиатуру для страницы книги
+def create_pagination_keyboard(*buttons: str) -> InlineKeyboardMarkup:
+    # Инициализируем билдер
     kb_builder = InlineKeyboardBuilder()
-    buttons: list[InlineKeyboardButton] = []
-    torrents = results.get('result')
-
-    for torrent in torrents:
-        text = (
-            f"Название: {torrent['title'][:20]}"
-            f"Автор: {torrent['author']}\n"
-            f"Категория: {torrent['category']}\n"
-            f"Сиды: {torrent['seeds']}\n"
-            f"Личи: {torrent['leeches']}\n"
-            f"Скачиваний: {torrent['downloads']}"
-        )
-        torrent_info_btn = InlineKeyboardButton(
-            text=text,
-            callback_data=f'torrent_{torrent['topic_id']}'
-        )
-        dwnld_btn = InlineKeyboardButton(
+    kb_builder.row(
+        InlineKeyboardButton(
+            text='Отмена',
+            callback_data='cancel'
+        ),
+        InlineKeyboardButton(
             text='Скачать',
-            callback_data=torrent['url'])
-        buttons.append(torrent_info_btn)
-    kb_builder.row(*buttons, width=1)
+            callback_data='download'
+            ),
+        width=2
+        )
+    # Добавляем в билдер ряд с кнопками
+    kb_builder.row(*[InlineKeyboardButton(
+        text=LEXICON[button] if button in LEXICON else button,
+        callback_data=button) for button in buttons])
+    # Возвращаем объект инлайн-клавиатуры
     return kb_builder.as_markup()
+
+
+# Функция для настройки кнопки Menu бота
+async def set_main_menu(bot: Bot):
+    main_menu_commands = [BotCommand(
+        command=command,
+        description=description
+    ) for command, description in LEXICON_COMMANDS.items()]
+    await bot.set_my_commands(main_menu_commands)
