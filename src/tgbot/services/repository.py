4e6656947575d96131494381b77fb9f6807c7394
aliceglass <abayhanov1998@gmail.com>
2024-06-03 20:01:07 +0300

    Добавил функции для получения для обновления и получения параметров поиска и отображения результатов. Также добавил функцию для получения всех callback_data у кнопок из меню настроек поиска. Возможно нужно сделать как-то по-другому.

--- src/tgbot/services/repository.py
+++ src/tgbot/services/repository.py
@@ -1,5 +1,7 @@
 from typing import List
 
+from tgbot.settings.settings import DisplaySetting, SearchSetting
+
 
 class Repo:
     """Db abstraction layer"""
@@ -24,3 +26,29 @@ class Repo:
             #     "select userid from tg_users",
             # )
         ]
+
+
+def update_search_params(search_settings: SearchSetting):
+    search_params = {
+        'sort': search_settings.sort,
+        'order': search_settings.order,
+        'category': search_settings.category,
+    }
+    return search_params
+
+
+def update_display_params(display_settings: DisplaySetting):
+    display_params = {
+        'display_mode': display_settings.display_mode,
+    }
+    return display_params
+
+
+def get_search_settings_callback_data() -> list[str]:
+    watch_res_callback_data = [
+        'display_mode', 'sort', 'order', 'sort_seeds', 'sort_leeches',
+        'sort_downloads', 'sort_registered', 'order_desc', 'order_asc',
+        'display_list', 'display_card'
+    ]
+
+    return watch_res_callback_data
