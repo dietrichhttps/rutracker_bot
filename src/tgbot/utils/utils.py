f7ea91fc62460f01c8695862ef0a1e78ec1f3237
aliceglass <abayhanov1998@gmail.com>
2024-06-04 17:01:54 +0300

    Создал универсальные функции для рендеринга страницы страницы настроек поиска и для обработки нажатий кнопок пагинации

--- src/tgbot/utils/utils.py
+++ src/tgbot/utils/utils.py
@@ -1,11 +1,13 @@
-from aiogram.types import CallbackQuery
+from typing import Union
+from aiogram.types import CallbackQuery, Message
 from aiogram.fsm.context import FSMContext
 
 from rutracker_api.main import RutrackerApi
 
 from tgbot.settings.settings import DisplaySetting, SearchSetting
 from tgbot.keyboards import keyboards
-from tgbot.services.text_service import get_torrent_info_text
+from tgbot.services.text_service import (generate_search_results_summary,
+                                         get_torrent_info_text)
 from tgbot.states.user import TorrentFSM
 
 
@@ -15,7 +17,7 @@ async def update_watch_results_page(callback: CallbackQuery,
                                     total_pages: int,
                                     display_mode: str,
                                     torrents_in_page: int | None = 10):
-    """Возвращает меню с результатами поиска"""
+    """Возвращает пользователю страницу с результатами поиска"""
     state_data = await state.get_data()
     torrents = state_data.get('torrents')
     if display_mode == 'card':
@@ -44,6 +46,31 @@ async def update_watch_results_page(callback: CallbackQuery,
             list_current_page=current_page, list_total_pages=total_pages,
             )
         await state.set_state(TorrentFSM.watch_results_list_page)
+
+
+async def update_search_settings_page(update: Union[Message, CallbackQuery],
+                                      search_results_info: dict,
+                                      search_params: dict,
+                                      display_params: dict,
+                                      global_current_page: int,
+                                      global_total_pages: int) -> None:
+    """"Возвращает пользователю страницу настроек поиска"""
+    if isinstance(update, Message):
+        await update.answer(
+            text=generate_search_results_summary(search_results_info),
+            reply_markup=keyboards.create_search_settings_kb(
+                search_params, display_params,
+                global_current_page, global_total_pages
+            )
+        )
+    else:
+        await update.message.edit_text(
+            text=generate_search_results_summary(search_results_info),
+            reply_markup=keyboards.create_search_settings_kb(
+                search_params, display_params,
+                global_current_page, global_total_pages
+            )
+        )
 
 
 async def handle_search_settings_change(
@@ -112,36 +139,68 @@ async def update_sort_settings(callback: CallbackQuery, state: FSMContext,
 
 
 async def update_search_request(query: str, search_settings: SearchSetting,
-                                api: RutrackerApi) -> tuple:
+                                api: RutrackerApi, page: int = 1) -> tuple:
     """Делает поисковый запрос и возвращает результат"""
     search_results_info = api.search(
         query,
         sort=search_settings.sort,
         order=search_settings.order,
+        page=page
     )
     return search_results_info
 
 
-async def handle_pagination_button_common(callback: CallbackQuery,
-                                          state: FSMContext,
-                                          current_page_key: str,
-                                          total_pages_key: str,
-                                          display_mode: str,
-                                          is_forward: bool):
-    """Общая функция для обработки нажатий на кнопки пагинации"""
-    state_data = await state.get_data()
-    prev_page = state_data.get(current_page_key)
-    total_pages = state_data.get(total_pages_key)
+async def handle_pagination_button_common(
+        callback: CallbackQuery,
+        state: FSMContext,
+        is_forward: bool,
+        search_settings: SearchSetting | None = None,
+        api: RutrackerApi | None = None):
+    """Универсальная функция для обработки нажатия кнопок пагинации"""
+    current_state = await state.get_state()
 
-    if is_forward:
-        condition = prev_page >= 0 and prev_page < total_pages
-        new_page = prev_page + 1
-    else:
-        condition = prev_page > 1 and prev_page <= total_pages
-        new_page = prev_page - 1
+    pagination_configs = {
+        'TorrentFSM:watch_results_card_page': (
+            'card_current_page', 'card_total_pages', 'card'
+            ),
+        'TorrentFSM:watch_results_list_page': (
+            'list_current_page', 'list_total_pages', 'list'
+            ),
+        'TorrentFSM:search_settings_page': (
+            'global_current_page', 'global_total_pages', None
+            )
+    }
 
-    if condition:
-        await update_watch_results_page(callback, state, new_page,
-                                        total_pages, display_mode)
-    else:
-        await callback.answer()
+    if current_state in pagination_configs:
+        current_page_key, total_pages_key, display_mode = pagination_configs[
+            current_state
+            ]
+        state_data = await state.get_data()
+        prev_page = state_data.get(current_page_key)
+        total_pages = state_data.get(total_pages_key)
+
+        new_page = prev_page + 1 if is_forward else prev_page - 1
+        condition = 0 < new_page <= total_pages
+
+        if not condition:
+            await callback.answer()
+            return
+
+        if current_page_key in ['card_current_page', 'list_current_page']:
+            await update_watch_results_page(callback, state, new_page,
+                                            total_pages, display_mode)
+        elif current_page_key == 'global_current_page':
+            query = state_data['query']
+            search_results_info = await update_search_request(query,
+                                                              search_settings,
+                                                              api,
+                                                              page=new_page)
+            torrents = search_results_info.get('result')
+            search_params = state_data.get('search_params')
+            display_params = state_data.get('display_params')
+            await state.update_data(search_results_info=search_results_info,
+                                    torrents=torrents,
+                                    global_current_page=new_page)
+            await update_search_settings_page(callback, search_results_info,
+                                              search_params, display_params,
+                                              new_page, total_pages)
